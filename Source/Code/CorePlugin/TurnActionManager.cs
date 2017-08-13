f85e8aa1ba44430d05df5b89379e733216bb2f62
mfep <serfozo.lorinc@gmail.com>
2017-08-13 21:13:14 +0200

    everything moves in the same frame

--- Source/Code/CorePlugin/TurnActionManager.cs
+++ Source/Code/CorePlugin/TurnActionManager.cs
@@ -22,20 +22,25 @@ namespace LowResRoguelike
 				return;
 			}
 
-			var objToMove = notMovedSet.First ();
-			foreach (var moveCmp in notMovedSet) {
-				if (moveCmp.Initiative > objToMove.Initiative) {
-					objToMove = moveCmp;
+			while (notMovedSet.Count > 0) {
+				var objToMove = notMovedSet.First ();
+				foreach (var moveCmp in notMovedSet) {
+					if (moveCmp.Initiative > objToMove.Initiative) {
+						objToMove = moveCmp;
+					}
 				}
-			}
 
-			var decision = objToMove.MakeDecision ();
-			var dirVector = decision.ToDirection ();
+				var decision = objToMove.MakeDecision ();
+				if (decision == Decision.NotDecided) {
+					break;
+				}
+				var dirVector = decision.ToDirection ();
 
-			var moveComp = objToMove as Component;
-			moveComp.GameObj.GetComponent<DiscreteTransform> ().MoveBy (dirVector);
-			if (decision != Decision.NotDecided) {
-				notMovedSet.Remove (objToMove);
+				var moveComp = objToMove as Component;
+				moveComp.GameObj.GetComponent<DiscreteTransform> ().MoveBy (dirVector);
+				if (decision != Decision.NotDecided) {
+					notMovedSet.Remove (objToMove);
+				}
 			}
 		}
 
