983fa02bc634547190657b469d8b9a8134b08ee1
mfep <serfozo.lorinc@gmail.com>
2017-08-22 21:42:58 +0200

    ItemGenerator bugfix

--- Source/Code/CorePlugin/ItemSystem/ItemGenerator.cs
+++ Source/Code/CorePlugin/ItemSystem/ItemGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Duality;
 using LowResRoguelike.GamePrefs;
@@ -9,7 +10,7 @@ namespace LowResRoguelike.ItemSystem
 		public static ItemInstance Generate (int lowestMaterial, int highestMaterial)
 		{
 			var slot = (ItemSlot)MathF.Rnd.Next (0, (int)ItemSlot.Last + 1);
-			var material = GetRandomFromList (PrefLoader.Materials);
+			var material = GetRandomFromList (PrefLoader.Materials, lowestMaterial, highestMaterial);
 
 			if (slot == ItemSlot.WeaponHand) {
 				var weaponClass = GetRandomFromList (PrefLoader.Weapons);
@@ -32,9 +33,9 @@ namespace LowResRoguelike.ItemSystem
 			return PrefLoader.PlayerStartingItems;
 		}
 
-		private static T GetRandomFromList<T> (IReadOnlyList<T> list)
+		private static T GetRandomFromList<T> (IReadOnlyList<T> list, int min = 0, int max = int.MaxValue - 1)
 		{
-			var idx = MathF.Rnd.Next (list.Count);
+			var idx = MathF.Rnd.Next (min, Math.Min (list.Count, max + 1));
 			return list[idx];
 		}
 	}
